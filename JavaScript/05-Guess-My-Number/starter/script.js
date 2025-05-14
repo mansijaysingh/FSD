c452dcd9c95b77da674faeb8c59f8d42c73eb846
Mansijaysingh <mansisingh.am000@gmail.com>
2025-05-14 13:58:09 +0530

    add a eventlistener to check button

--- JavaScript/05-Guess-My-Number/starter/script.js
+++ JavaScript/05-Guess-My-Number/starter/script.js
@@ -1,8 +1,17 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent="Correct Number!";
-document.querySelector('.number').textContent=13;
-document.querySelector('.score').textContent=20;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent="Correct Number!";
+// document.querySelector('.number').textContent=13;
+// document.querySelector('.score').textContent=20;
 
-document.querySelector('.guess').value=23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value=23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click',function(){
+ const guess= Number(document.querySelector('.guess').value);
+ console.log( typeof guess)
+
+ if(!guess){
+  document.querySelector('.message').textContent='No Number!';
+ }
+})
