7a6729ec84f753dadc7e3434df10286abda8a321
Mansijaysingh <mansisingh.am000@gmail.com>
2025-05-16 11:55:36 +0530

    change background color

--- JavaScript/05-Guess-My-Number/starter/script.js
+++ JavaScript/05-Guess-My-Number/starter/script.js
@@ -7,11 +7,20 @@
 // document.querySelector('.guess').value=23;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber=Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent=secretNumber;
+
 document.querySelector('.check').addEventListener('click',function(){
  const guess= Number(document.querySelector('.guess').value);
  console.log( typeof guess)
 
  if(!guess){
-  document.querySelector('.message').textContent='No Number!';
+  document.querySelector('.message').textContent='❌ No Number!';
+ }else if(guess==secretNumber){
+  document.querySelector('.message').textContent='🎉Correct Number!';
+ }else if(guess>secretNumber){
+  document.querySelector('.message').textContent='📈Too High!';
+ }else if(guess<secretNumber){
+  document.querySelector('.message').textContent='📉Too Low!';
  }
 })
