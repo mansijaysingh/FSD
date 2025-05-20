e1cff690be817b04f8508ad6957096898731dfb6
Mansijaysingh <mansisingh.am000@gmail.com>
2025-05-20 11:06:48 +0530

    implement highscore

--- JavaScript/05-Guess-My-Number/starter/script.js
+++ JavaScript/05-Guess-My-Number/starter/script.js
@@ -7,20 +7,70 @@
 // document.querySelector('.guess').value=23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber=Math.trunc(Math.random()*20)+1;
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let highscore=0;
+
 document.querySelector('.number').textContent=secretNumber;
 
 document.querySelector('.check').addEventListener('click',function(){
  const guess= Number(document.querySelector('.guess').value);
  console.log( typeof guess)
 
+ //when there is no input
  if(!guess){
   document.querySelector('.message').textContent='❌ No Number!';
+
+  //when player wins
  }else if(guess==secretNumber){
   document.querySelector('.message').textContent='🎉Correct Number!';
+
+  document.body.style.backgroundColor='#60b347';
+  document.querySelector('.number').style.width='30rem';
+if(score>highscore){
+  highscore=score;
+document.querySelector('.highscore').textContent=highscore;
+}
+
+document.querySelector('.message').style.color='blue';
+
+
+
+//when guess is too high
  }else if(guess>secretNumber){
-  document.querySelector('.message').textContent='📈Too High!';
+  if(score>0){
+     document.querySelector('.message').textContent='📈Too High!';
+     score--;
+  document.querySelector('.score').textContent=score;
+  }else{
+    document.querySelector('.message').textContent='💥 You lost the game!';
+    
+    // document.querySelector('.score').textContent=0;
+  }
+ 
+  //when guess is too low
  }else if(guess<secretNumber){
-  document.querySelector('.message').textContent='📉Too Low!';
+  if(score>0){document.querySelector('.message').textContent='📉Too Low!';
+  score--;
+  document.querySelector('.score').textContent=score;
+}else{
+    document.querySelector('message').textContent='💥 You lost the game!';
+  
+  }
  }
 })
+
+//when player clicks again button
+document.querySelector('.again').addEventListener('click',function(){
+  score=20;
+  secretNumber=Math.trunc(Math.random()*20)+1;
+
+  document.querySelector('.message').textContent='Start guessing...';
+  document.querySelector('.score').textContent=score;
+  document.querySelector('.number').textContent='?';
+  document.querySelector('.guess').value='';
+  document.body.style.backgroundColor='#074e57';
+  document.querySelector('.number').style.width='15rem';
+})
+
+//Implementing Highscore
