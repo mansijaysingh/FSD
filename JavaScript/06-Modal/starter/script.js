a254f283329f4db89fa56306012ccec8a5fe58bb
Mansijaysingh <mansisingh.am000@gmail.com>
2025-05-20 12:23:52 +0530

    complete a simple project

--- JavaScript/06-Modal/starter/script.js
+++ JavaScript/06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal =document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnsOpenModal=document.querySelectorAll('.show-modal');
+
+const openModal=function(){
+   modal.classList.remove('hidden');
+   overlay.classList.remove('hidden');
+}
+
+const closedModal=function(){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+// console.log(btnsOpenModal);
+
+for(let i=0; i<btnsOpenModal.length; i++){
+  btnsOpenModal[i].addEventListener('click',openModal);
+}
+btnCloseModal.addEventListener('click',closedModal);
+overlay.addEventListener('click',closedModal)
