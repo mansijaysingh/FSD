4ffdd2aca5b2f49d112524c8b3d05d93b52d0e41
Mansijaysingh <mansisingh.am000@gmail.com>
2025-05-21 10:41:37 +0530

    add reset game button

--- JavaScript/07-Pig-Game/starter/script.js
+++ JavaScript/07-Pig-Game/starter/script.js
@@ -12,15 +12,38 @@ const btnNew=document.querySelector('.btn--new');
 const btnRoll=document.querySelector('.btn--roll'); 
 const btnHold=document.querySelector('.btn--hold');
 
+let  scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-score0El.textContent=0;
+
+function init(){
+   score0El.textContent=0;
 score1El.textContent=0;
+current0El.textContent=0;
+current1El.textContent=0;
+player0El.classList.remove('player--winner');
+player1El.classList.remove('player--winner');
+player0El.classList.add('player--active');
+player1El.classList.remove('player--active');
 diceEl.classList.add('hidden');
 
-const scores=[0,0];
-let currentScore=0;
-let activePlayer=0;
-let playing=true;
+  scores=[0,0];
+  currentScore=0;
+  activePlayer=0;
+  playing=true;
+
+
+document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+
+document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+}
+init();
+// score0El.textContent=0;
+// score1El.textContent=0;
+// diceEl.classList.add('hidden');
+
 
 
 const switchPlayer=function(){
@@ -76,8 +99,8 @@ switchPlayer();
 }
 }
 
-
-
-
 });
 
+
+//resetting the game
+btnNew.addEventListener('click',init);
