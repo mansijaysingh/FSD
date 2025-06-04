1aca357b9d4e1ad178239e60143ed0ce62975070
Mansijaysingh <mansisingh.am000@gmail.com>
2025-06-04 14:52:18 +0530

    done

--- projects/Amozon clone/script.js
+++ projects/Amozon clone/script.js
@@ -5,5 +5,34 @@ const next=document.querySelector('.control-next');
 let n=0;
 
 function changeImg(){
-  
+  for(let i=0; i<imgs.length; i++){
+    imgs[i].style.display='none';
+  }
+  imgs[n].style.display='block';
 }
+changeImg();
+
+ prev.addEventListener('click',function(){
+  n--;
+  if(n<0){
+    n=imgs.length-1;
+  }
+  changeImg();
+ })
+
+ next.addEventListener('click',function(){
+  n++;
+  if(n>=imgs.length){
+    n=0;
+  }
+  changeImg();
+ })
+
+ const scrollContainer=document.querySelectorAll('.products');
+
+ for(const container of scrollContainer){
+  container.addEventListener('wheel',(evt)=>{
+    evt.preventDefault();
+    container.scrollLeft += evt.deltaY;
+  })
+ }
