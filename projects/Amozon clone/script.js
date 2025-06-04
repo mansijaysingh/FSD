1af0977183390f04c2c6705a94535a77be284b1c
Mansijaysingh <mansisingh.am000@gmail.com>
2025-06-04 12:49:26 +0530

    ..

---
+++ projects/Amozon clone/script.js
@@ -0,0 +1,9 @@
+const imgs=document.querySelectorAll('.header-slider ul img');
+const prev=document.querySelector('.control-prev');
+const next=document.querySelector('.control-next');
+
+let n=0;
+
+function changeImg(){
+  
+}
