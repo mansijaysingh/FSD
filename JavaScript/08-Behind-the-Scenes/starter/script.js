1614a0dd4b2d942fa3e5132a92a1195c084be991
Mansijaysingh <mansisingh.am000@gmail.com>
2025-06-26 11:48:31 +0530

    practice scoping

--- JavaScript/08-Behind-the-Scenes/starter/script.js
+++ JavaScript/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+function calcAge(birtheyear){
+  const age =2025-birtheyear
+ function printAge(){
+  const  output=`you are ${age},born in ${birtheyear}`
+  console.log(output);
+
+  if(birtheyear>=1981 && birtheyear<=1996){
+    const str=`you are a millennial, born in ${birtheyear}`
+  }
+ }
+ printAge();
+return age;
+}
+const firstName='john';
+calcAge(2002);
