71c198a79cbe842d167a129bf32ef707a4b73098
Mansijaysingh <mansisingh.am000@gmail.com>
2025-06-26 12:52:00 +0530

    hositing pratice

--- JavaScript/08-Behind-the-Scenes/starter/script.js
+++ JavaScript/08-Behind-the-Scenes/starter/script.js
@@ -1,21 +1,37 @@
 'use strict';
 
 
-function calcAge(birtheyear){
-  const age =2025-birtheyear
- function printAge(){
-  const  output=`you are ${age},born in ${birtheyear}`
-  console.log(output);
+// function calcAge(birtheyear){
+//   const age =2025-birtheyear
+//  function printAge(){
+//   const  output=`you are ${age},born in ${birtheyear}`
+//   console.log(output);
 
-  if(birtheyear>=1981 && birtheyear<=1996){
-    const str=`you are a millennial, born in ${birtheyear}`
+//   if(birtheyear>=1981 && birtheyear<=1996){
+//     const str=`you are a millennial, born in ${birtheyear}`
 
 
-    
-  }
- }
- printAge();
-return age;
-}
-const firstName='john';
-calcAge(2002);
+
+//   }
+//  }
+//  printAge();
+// return age;
+// }
+// const firstName='john';
+// calcAge(2002);
+
+// console.log(me);
+// console.log(job);
+// console.log(year)
+
+
+// var me ="mansi";
+// let job="teacher";
+// const year=2002;
+
+
+//functions
+// function addDecl(a,b){
+//   return a+b;
+// }
+
